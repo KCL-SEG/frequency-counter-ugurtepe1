dbf375130be0750d01a636e6ab7efec4e9b66f9f
Ugur Tepe'git config --global user.name Ugur <ugur.tepe@kcl.ac.uk>
2023-10-03 01:26:38 +0100

    did the assignment on the frequencies.py file.

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for item in items:
+        if str(item) in frequencies:
+            frequencies[str(item)] += 1
+        else:
+            frequencies[str(item)] = 1 
     return frequencies
+   
